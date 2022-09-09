bb1d15936ea8b08d2943f9ac1a740bf4825bb7c1
thayna <3therer@gmail.com>
2022-09-09 12:53:36 -0300

    criado model e service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +22,9 @@ import { ContatoComponent } from './contato/contato.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-cadastrar',
@@ -8,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
 export class CadastrarComponent implements OnInit {
   isCpf:boolean = true
 
-  constructor() {}
+  Usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUser: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) 
+    
+    { }
+  
 
   ngOnInit() {
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+
+  }
+
+  tipoUsuario(event:any){
+    this.tipoUser = event.target.value
+
+  }
+
+  cadastrar(){
+    this.Usuario.tipo = this.tipoUser
+
+    if(this.Usuario.senha != this.confirmarSenha){
+      alert('senhas incorretas.')
+    
+    } else {
+      this.authService.cadastrar(this.Usuario).subscribe((resp: Usuario) => {
+        this.Usuario = resp
+        this.router.navigate(['/entrar'])
+
+
+        alert('Usuário cadastrado com sucesso.')
+      })
+
+    }
+  
+    
+  }
+
   onChangeCheckBox(){
     if(this.isCpf == false){
       this.isCpf=true
